615323614da7f1507bb06893e7a651a6ec4d053b
Sebastian Sejud <ssejud@gmail.com>
2020-10-31 20:38:17 +0100

    ...

---
+++ CrossX/CrossX/Graphics/TextureSamplerDesc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CrossX.Graphics
+{
+    [Flags]
+    public enum TextureSamplerDesc
+    {
+        Point = 1,
+        Linear = 2,
+        Anisotropic = 3,
+
+        WrapU = 0,
+        WrapV = 0,
+
+        ClampU = 4,
+        ClampV = 8,
+
+        MirrorU = 16,
+        MirrorV = 32
+    }
+}
